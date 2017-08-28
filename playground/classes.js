582fec3b854406c3bf7e45032fd88adc86dd698c
nnattawat <armmer1@gmail.com>
2017-08-28 12:12:56 +1000

    Inherit class

--- playground/classes.js
+++ playground/classes.js
@@ -26,3 +26,24 @@ console.log(person2);
 console.log(person2.getDescription());
 // Person { name: 'Nattawat', age: 20 }
 // I'm Nattawat and I'm 20!
+
+class Child extends Person {
+  constructor({name="Anonymous", age=0, like}) {
+    super({name, age});
+
+    this.like = like;
+  }
+  // overridden methods
+  getGreeting() {
+    return `I'm ${this.name}.... I like ${this.like}`
+  }
+}
+
+let child1 = new Child({name: 'Nattawat', age: 20, like: 'football'});
+
+console.log(child1);
+console.log(child1.getGreeting());
+console.log(child1.getDescription());
+// Child { name: 'Nattawat', age: 20 }
+// I'm Nattawat.... I like football
+// I'm Nattawat and I'm 20!
