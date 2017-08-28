62d6cc81e09af6d28178c936e3c4238db593ebe1
nnattawat <armmer1@gmail.com>
2017-08-28 12:26:04 +1000

    Work with state

--- app/components/ComponentOne.jsx
+++ app/components/ComponentOne.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 let ComponentOne = React.createClass({
+  getInitialState() {
+    return {
+      count: 1
+    }
+  },
+
   render() {
     return (
       <div>
         <h3>ComponentOne using React.createClass</h3>
+        <p>Current count: {this.state.count}</p>
       </div>
     )
   }
--- app/components/ComponentTwo.jsx
+++ app/components/ComponentTwo.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 class ComponentTwo extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 2
+    }
+  }
+
   render() {
     return (
       <div>
         <h3>ComponentTwo extending React.Component</h3>
+        <p>Current count: {this.state.count}</p>
       </div>
     )
   }
