dff983daabb0dac2322e87f9ea4835fa2505714a
nnattawat <armmer1@gmail.com>
2017-08-28 12:21:07 +1000

    Add React components using es6 extends

---
+++ app/components/ComponentOne.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let ComponentOne = React.createClass({
+  render() {
+    return (
+      <div>
+        <h3>ComponentOne using React.createClass</h3>
+      </div>
+    )
+  }
+});
+
+export default ComponentOne;
---
+++ app/components/ComponentTwo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ComponentTwo extends React.Component{
+  render() {
+    return (
+      <div>
+        <h3>ComponentTwo extending React.Component</h3>
+      </div>
+    )
+  }
+};
+
+export default ComponentTwo;
